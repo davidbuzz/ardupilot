21e8a5b27e56e1a1d4d8981e67f47f90f9e74ba9
Buzz <davidbuzz@gmail.com>
2019-03-18 20:18:58 +1000

    push version details

--- ArduPlane/version.h
+++ ArduPlane/version.h
@@ -6,7 +6,7 @@
 
 #include "ap_version.h"
 
-#define THISFIRMWARE "ArduPlane V3.9.6"
+#define THISFIRMWARE "ArduPlane V3.9.6-rfdesign-boeing-rebase2"
 
 // the following line is parsed by the autotest scripts
 #define FIRMWARE_VERSION 3,9,6,FIRMWARE_VERSION_TYPE_OFFICIAL
