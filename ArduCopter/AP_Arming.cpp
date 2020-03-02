f65d8d88a29819eb7239585bce8579ff74a9b2e2
Buzz <davidbuzz@gmail.com>
2020-03-02 12:15:45 +1000

    AP_Arming: Updated pre-arm checks to not test calibration when we are feeding data we know is correct

--- ArduCopter/AP_Arming.cpp
+++ ArduCopter/AP_Arming.cpp
@@ -96,11 +96,13 @@ bool AP_Arming_Copter::compass_checks(bool display_failure)
     if ((checks_to_perform == ARMING_CHECK_ALL) || (checks_to_perform & ARMING_CHECK_COMPASS)) {
         // check compass offsets have been set.  AP_Arming only checks
         // this if learning is off; Copter *always* checks.
+#if CONFIG_HAL_BOARD_SUBTYPE != HAL_BOARD_SUBTYPE_SITL_CAN
         char failure_msg[50] = {};
         if (!AP::compass().configured(failure_msg, ARRAY_SIZE(failure_msg))) {
             check_failed(ARMING_CHECK_COMPASS, display_failure, "%s", failure_msg);
             ret = false;
         }
+#endif
     }
 
     return ret;
