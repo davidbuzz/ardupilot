13f4c691df6db61f00df7e866521cbd21cd714e4
Buzz <davidbuzz@gmail.com>
2022-05-14 20:45:58 +1000

    not sure which if these, but one of them stopped the constant reboots.

--- libraries/AP_HAL_ESP32/boards/esp32buzz.h
+++ libraries/AP_HAL_ESP32/boards/esp32buzz.h
@@ -164,8 +164,8 @@
 //#define HAL_ESP32_SPI_DEVICES {}
 
 //I2C bus list
-//#define HAL_ESP32_I2C_BUSES	{.port=I2C_NUM_0, .sda=GPIO_NUM_13, .scl=GPIO_NUM_12, .speed=400*KHZ, .internal=true}
-#define HAL_ESP32_I2C_BUSES {} // using this embty block appears to cause crashes?
+#define HAL_ESP32_I2C_BUSES	{.port=I2C_NUM_0, .sda=GPIO_NUM_13, .scl=GPIO_NUM_12, .speed=400*KHZ, .internal=true}
+//#define HAL_ESP32_I2C_BUSES {} // using this embty block appears to cause crashes?
 
 
 // rcin on what pin?
