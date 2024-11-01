6d113d6a0edd0c4ec6a42299bc5f11c7a04531db
David Buzz <davidbuzz@gmail.com>
2024-11-01 13:58:44 +1000

    esp32 and HAL_PARAM_DEFAULTS_PATH redef

--- libraries/AP_HAL_ESP32/RCOutput.h
+++ libraries/AP_HAL_ESP32/RCOutput.h
@@ -21,7 +21,9 @@
 
 #include <AP_HAL/RCOutput.h>
 #include "HAL_ESP32_Namespace.h"
+#ifndef HAL_PARAM_DEFAULTS_PATH
 #define HAL_PARAM_DEFAULTS_PATH nullptr
+#endif
 #include <AP_HAL/Util.h>
 
 #include "driver/gpio.h"
