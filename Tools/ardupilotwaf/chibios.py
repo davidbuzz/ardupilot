816454bed82e7a2fd947b0fc2e7dd25bc660d7dd
David 'Buzz' Bussenschutt <d.bussenschutt@uq.edu.au>
2026-03-13 09:38:53 +1000

    Pico2/RP2350: implement --upload flag via picotool UF2 WIP
    Add upload_fw_pico2 waf task class that handles firmware upload for
    RP2350/Pico2 boards via picotool:
    - Auto-downloads picotool from pico-sdk-tools releases if not present
    - Converts ELF to UF2 using 'picotool uf2 convert -t elf'
    - Uploads with 'picotool load -v -x <uf2> -f' (requires BOOTSEL mode)
    
    The upload trigger in chibios_firmware() branches on board name:
    Pico2 uses upload_fw_pico2 (ELF->UF2->picotool), all other ChibiOS
    boards continue to use the existing upload_fw (uploader.py/STM32).
    
    Also add picotool/ and picotool.tar.gz to .gitignore.

--- Tools/ardupilotwaf/chibios.py
+++ Tools/ardupilotwaf/chibios.py
@@ -138,6 +138,61 @@ class upload_fw(Task.Task):
     def keyword(self):
         return "Uploading"
 
+
+class upload_fw_pico2(Task.Task):
+    '''Upload firmware to RP2350/Pico2 using picotool'''
+    color = 'BLUE'
+    always_run = True
+
+    PICOTOOL_URL = 'https://github.com/raspberrypi/pico-sdk-tools/releases/download/v2.2.0-3/picotool-2.2.0-a4-x86_64-lin.tar.gz'
+
+    def ensure_picotool(self):
+        '''Download and extract picotool if not already present.
+        The tarball structure is picotool/picotool, so we extract to SRCROOT
+        and the binary lands at SRCROOT/picotool/picotool.'''
+        import urllib.request
+        import tarfile
+        srcroot = self.env.get_flat('SRCROOT')
+        picotool_path = os.path.join(srcroot, 'picotool', 'picotool')
+        if not os.path.exists(picotool_path):
+            tarball = os.path.join(srcroot, 'picotool.tar.gz')
+            print("Downloading picotool from %s ..." % self.PICOTOOL_URL)
+            urllib.request.urlretrieve(self.PICOTOOL_URL, tarball)
+            with tarfile.open(tarball) as tar:
+                tar.extractall(srcroot)
+            os.remove(tarball)
+            if not os.path.exists(picotool_path):
+                raise Exception("picotool binary not found at %s after extraction" % picotool_path)
+            os.chmod(picotool_path, 0o755)
+        return picotool_path
+
+    def run(self):
+        elf_path = self.inputs[0].abspath()
+        # Place UF2 alongside the ELF in the bin/ directory
+        uf2_path = os.path.splitext(elf_path)[0] + '.uf2'
+        if not uf2_path.endswith('.uf2'):
+            uf2_path = elf_path + '.uf2'
+
+        picotool = self.ensure_picotool()
+
+        # Convert ELF to UF2; use -t elf since the file has no .elf extension
+        print("Converting ELF to UF2: %s -> %s" % (elf_path, uf2_path))
+        ret = self.exec_command([picotool, 'uf2', 'convert', elf_path, '-t', 'elf', uf2_path])
+        if ret != 0:
+            print("Error: picotool uf2 convert failed")
+            return ret
+
+        print("\nUploading to Pico2/RP2350...")
+        print("NOTE: Board must be in BOOTSEL mode (hold BOOTSEL button while connecting USB)\n")
+        return self.exec_command([picotool, 'load', '-v', '-x', uf2_path, '-f'])
+
+    def exec_command(self, cmd, **kw):
+        kw['stdout'] = sys.stdout
+        return super(upload_fw_pico2, self).exec_command(cmd, **kw)
+
+    def keyword(self):
+        return "Uploading (Pico2)"
+
 class set_default_parameters(Task.Task):
     color='CYAN'
     always_run = True
@@ -481,8 +536,12 @@ def chibios_firmware(self):
             hex_task.set_run_after(generate_bin_task)
         
     if self.bld.options.upload:
-        _upload_task = self.create_task('upload_fw', src=apj_target)
-        _upload_task.set_run_after(generate_apj_task)
+        if 'pico2' in self.env.BOARD.lower():
+            _upload_task = self.create_task('upload_fw_pico2', src=link_output)
+            _upload_task.set_run_after(generate_apj_task)
+        else:
+            _upload_task = self.create_task('upload_fw', src=apj_target)
+            _upload_task.set_run_after(generate_apj_task)
 
     if self.bld.options.upload_blueos:
         _upload_task = self.create_task('upload_fw_blueos', src=link_output)
