88dc376d2a9c984c639c1f951dfb7e2b1014d9c9
David Buzz <davidbuzz@gmail.com>
2026-03-28 16:13:26 +1000

    keep bootloader single core otherwise we can accidentially run the wrong stuff.

--- libraries/AP_HAL_ChibiOS/hwdef/common/rp2350_mcuconf.h
+++ libraries/AP_HAL_ChibiOS/hwdef/common/rp2350_mcuconf.h
@@ -38,7 +38,9 @@
  * HAL driver system settings.
  */
 #define RP_NO_INIT                          FALSE
+#ifndef RP_CORE1_START
 #define RP_CORE1_START                      TRUE
+#endif
 #define RP_CORE1_VECTORS_TABLE              _vectors
 #define RP_CORE1_ENTRY_POINT                _crt0_c1_entry
 #define RP_CORE1_STACK_END                  __c1_main_stack_end__
