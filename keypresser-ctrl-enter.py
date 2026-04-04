1df7bf4e59475e10d2fcb77a6d0d6fc13138cd10
David Buzz <davidbuzz@gmail.com>
2026-04-05 08:16:57 +1000

    simplify

--- keypresser-ctrl-enter.py
+++ keypresser-ctrl-enter.py
@@ -17,16 +17,16 @@ import time
 import keyboard
 secs_counter=0
 while True:
-    print ("Pressing ctrl-enter...")
+    print ("Pressing ctrl+enter...")
     keyboard.press_and_release('ctrl+enter')
     time.sleep(5)
     secs_counter += 5
-    if secs_counter >= 3600:
-        print ("typing 'please continue analysis' and then enter...")
-        #keyboard.write('go-ahead to finish PIOUART RX/TX end-to-end on hardware')
-        keyboard.write('continue analysis')
-        keyboard.press_and_release('enter')
-        secs_counter = 0
+    #if secs_counter >= 3600:
+    #    print ("typing 'please continue analysis' and then enter...")
+    #    #keyboard.write('go-ahead to finish PIOUART RX/TX end-to-end on hardware')
+    #    keyboard.write('continue analysis')
+    #    keyboard.press_and_release('enter')
+    #    secs_counter = 0
 
 # needs run with sudo
 
