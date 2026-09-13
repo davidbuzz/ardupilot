fb0e48f50735dc93d9e29dae0586e60864bb8c39
Andy Piper <github@andypiper.com>
2026-09-13 10:09:35 +0100

    waf: report a failed RP2350 section generator in one line
    Without an error message waf reports the task by its repr, which lists
    every program object and buries the registry miss printed just above.

--- Tools/ardupilotwaf/chibios.py
+++ Tools/ardupilotwaf/chibios.py
@@ -146,7 +146,10 @@ class rp2350_ramfunc2_gen(Task.Task):
         buildroot = self.env.BUILDROOT
         script = os.path.join(self.env.SRCROOT,
                               'libraries/AP_HAL_ChibiOS/hwdef/common/rp2350_ramfunc2_sections.sh')
-        return subprocess.call(['bash', script, buildroot])
+        ret = subprocess.call(['bash', script, buildroot])
+        if ret:
+            self.err_msg = 'rp2350_ramfunc2_sections.sh failed, see above'
+        return ret
 
     def __str__(self):
         return 'rp2350_ramfunc2_sections.ld'
